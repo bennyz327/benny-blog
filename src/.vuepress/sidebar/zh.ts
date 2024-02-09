68adc73d2dc5d2fa61c183f6be9c9bd81ed109bf
benny-desktop <bennyz327@gmail.com>
2024-02-09 16:12:43 +0800

    修正側邊欄

--- src/.vuepress/sidebar/zh.ts
+++ src/.vuepress/sidebar/zh.ts
@@ -1,15 +1,15 @@
 import { sidebar } from "vuepress-theme-hope";
 
 export const zhSidebar = sidebar({
-  "/zh/": [
+  "/": [
     "",
-    {
-      text: "如何使用",
-      icon: "laptop-code",
-      prefix: "demo/",
-      link: "demo/",
-      children: "structure",
-    },
+    // {
+    //   text: "如何使用",
+    //   icon: "laptop-code",
+    //   prefix: "demo/",
+    //   link: "demo/",
+    //   children: "structure",
+    // },
     {
       text: "文章",
       icon: "book",
@@ -17,6 +17,5 @@ export const zhSidebar = sidebar({
       children: "structure",
     },
     "intro",
-    "slides",
   ],
 });
